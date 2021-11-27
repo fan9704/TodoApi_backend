e2b11e17093499748e4ec0485338fecd9ed6867a
fan9704 <cxz123499@gmail.com>
2021-11-27 16:39:04 +0800

    created Product but Cannot Migration

---
+++ Models/Product.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using TodoApi_backend.Data;
+namespace TodoApi_backend.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+       [Display(Name = "產品名稱")]
+        public string Name { get; set; }
+       [Display(Name = "類別")]
+        public string Type { get; set; }
+        [Display(Name = "數量")]
+        public int Quantity { get; set; }
+       [Display(Name = "購買日期")]
+        [DataType(DataType.Date)]
+        public DateTime PurchaseDate { get; set; }
+
+
+        [Display(Name = "銷售日期")]
+        [DataType(DataType.Date)]
+        public DateTime SellDate { get; set; }
+       [Display(Name = "成本")]
+        public int Cost { get; set; }
+        [Display(Name = "售價")]
+        public int Price { get; set; }
+    }
+}
